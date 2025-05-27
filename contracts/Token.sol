b58541307a8a8be460c827ba55a39522d750ae00
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2025-05-27 11:16:44 +0100

    added governance contracts

---
+++ contracts/Token.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.26;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Token is ERC20, Ownable, ERC20Permit, ERC20Votes {
+    constructor(string memory _name, string memory _symbol, address _initialOwner)
+        ERC20(_name, _symbol)
+        Ownable(_initialOwner)
+        ERC20Permit(_name)
+    {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._update(from, to, value);
+    }
+
+    function nonces(address owner)
+        public
+        view
+        override(ERC20Permit, Nonces)
+        returns (uint256)
+    {
+        return super.nonces(owner);
+    }
+}
