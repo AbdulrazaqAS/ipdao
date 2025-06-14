0e844f400495336740c841f80abe5351b18c02d5
Abdulrazaq Ahmad Sabo <93339361+AbdulrazaqAS@users.noreply.github.com>
2025-06-14 16:41:55 +0000

    removed rainbow wallet package

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { TomoEVMKitProvider } from '@tomo-inc/tomo-evm-kit';
-//import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WagmiProvider } from 'wagmi'
 import { config } from './utils/tomoconfig'
 
 import '@tomo-inc/tomo-evm-kit/styles.css';
-//import '@rainbow-me/rainbowkit/styles.css';
 
 const queryClient = new QueryClient()
 
